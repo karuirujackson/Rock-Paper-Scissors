414e5409eb94e7145d681f424f799ada7fc1afc0
karuirujackson <jkjackaruiru641@gmail.com>
2023-08-23 16:52:18 +0300

    The Rock-Paper-Scissors Game

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Game from './Game.js'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I Am a Front End Developer</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
